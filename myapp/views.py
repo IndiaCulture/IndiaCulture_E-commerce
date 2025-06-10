5171b8457f686e919435cbe165d90674606f201c
Hariprasath-003 <hariprasath6323@gmail.com>
2025-06-10 09:53:09 +0300

    upi fix 2

--- myapp/views.py
+++ myapp/views.py
@@ -152,7 +152,7 @@ def checkout_view(request):
     user = request.user
     
     # Define UPI ID (could be in settings or DB)
-    upi_id = '9003689821@ybl'  # replace with your UPI or fetch dynamically
+    upi_id = 'mtamilselvanmtamilselvan32@oksbi'  # replace with your UPI or fetch dynamically
     
 
     # Build UPI link with amount and payee name (URL-encoded)
